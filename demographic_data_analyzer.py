c85a2ca1fd145e6181c9d74dcc6586be50fbc6a6
William Allinson <w.allinson@outlook.com>
2024-10-22 21:38:13 +0000

    More conditions solved

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -13,26 +13,33 @@ def calculate_demographic_data(print_data=True):
     average_age_men = round(men['age'].mean(), 1)
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    edu = df['education'].value_counts()
+    percentage_bachelors = round((edu['Bachelors']/edu.sum())*100, 1)
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
 
     # with and without `Bachelors`, `Masters`, or `Doctorate`
-    higher_education = None
-    lower_education = None
+    degrees = ['Bachelors','Masters','Doctorate']
+    higher = df[df['education'].isin(degrees)]
+    lower = df[~df['education'].isin(degrees)]
+    higher_education = round((higher.shape[0]/df.shape[0])*100, 1)
+    lower_education = round((lower.shape[0]/df.shape[0])*100, 1)
 
     # percentage with salary >50K
-    higher_education_rich = None
-    lower_education_rich = None
+    richhigher = higher[higher['salary'] == '>50K']
+    richlower = lower[lower['salary'] == '>50K']
+    higher_education_rich = round((richhigher.shape[0]/higher.shape[0])*100, 1)
+    lower_education_rich = round((richlower.shape[0]/lower.shape[0])*100, 1)
 
     # What is the minimum number of hours a person works per week (hours-per-week feature)?
-    min_work_hours = None
+    min_work_hours = df['hours-per-week'].min()
 
     # What percentage of the people who work the minimum number of hours per week have a salary of >50K?
-    num_min_workers = None
-
-    rich_percentage = None
+    num_min_workers = df[df['hours-per-week'] == min_work_hours].shape[0]
+    lazys = df[df['hours-per-week'] == min_work_hours]
+    richlazys = (lazys[lazys['salary'] == '>50K']).shape[0]
+    rich_percentage = round((richlazys/lazys.shape[0])*100, 1)
 
     # What country has the highest percentage of people that earn >50K?
     highest_earning_country = None
--- testing.py
+++ testing.py
@@ -1,5 +1,18 @@
 import pandas as pd
 
 df = pd.read_csv('adult.data.csv')
-men = df[df['sex'] == 'Male']
-print(round(men['age'].mean(), 1))
+'''degrees = ['Bachelors','Masters','Doctorate']
+higher = df[df['education'].isin(degrees)]
+lower = df[~df['education'].isin(degrees)]
+richhigher = higher[higher['salary'] == '>50K']
+richlower = lower[lower['salary'] == '>50K']
+
+min_work_hours = df['hours-per-week'].min()
+lazys = df[df['hours-per-week'] == min_work_hours]
+#print(lazys)
+richlazys = (lazys[lazys['salary'] == '>50K']).shape[0]
+richlazypercent = round((richlazys/lazys.shape[0])*100, 1)
+print(richlazypercent)'''
+
+for country in df['native-country']:
+    print(country)
